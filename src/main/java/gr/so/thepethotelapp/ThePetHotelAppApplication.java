378acdf63cac75cafc333b82b0223b7c404469cd
ouraniasopidou <rania_sg@hotmail.com>
2025-02-11 00:30:50 +0200

    Improved security: Moved database credentials to .env file and updated .gitignore

--- src/main/java/gr/so/thepethotelapp/ThePetHotelAppApplication.java
+++ src/main/java/gr/so/thepethotelapp/ThePetHotelAppApplication.java
@@ -1,5 +1,6 @@
 package gr.so.thepethotelapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThePetHotelAppApplication {
 
 	public static void main(String[] args) {
+		// Φόρτωση των μεταβλητών περιβάλλοντος από το .env
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ThePetHotelAppApplication.class, args);
 	}
-
 }
